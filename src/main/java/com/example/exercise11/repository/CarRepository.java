66ca21ccd5cf83933d04208a8e2913cf28100860
Federico Maiello <federicomaiello@hotmail.it>
2024-03-11 11:47:33 +0100

    Repository1

--- src/main/java/com/example/exercise11/repository/CarRepository.java
+++ src/main/java/com/example/exercise11/repository/CarRepository.java
@@ -2,8 +2,8 @@ package com.example.exercise11.repository;
 
 import com.example.exercise11.entity.Car;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
 
-@RepositoryRestResource
+@Repository
 public interface CarRepository extends JpaRepository<Car,Long>{
 }
